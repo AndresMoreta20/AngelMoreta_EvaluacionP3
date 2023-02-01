237a724ff946cbca9f1d0b8cc94784f039486d69
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-02-01 08:55:30 -0500

    Se añade la carpeta ViewModel y la clase AMDateViewModel para almacenar la fecha actual.

--- AngelMoreta_EvaluacionP3/App.xaml.cs
+++ AngelMoreta_EvaluacionP3/App.xaml.cs
@@ -1,11 +1,14 @@
-﻿namespace AngelMoreta_EvaluacionP3;
+﻿using AngelMoreta_EvaluacionP3.Data;
+namespace AngelMoreta_EvaluacionP3;
 
 public partial class App : Application
 {
-	public App()
+    public static AMFraseDatabase AMFraseRepo { get; set; }
+    public App(AMFraseDatabase repo)
 	{
 		InitializeComponent();
 
 		MainPage = new AppShell();
+		AMFraseRepo= repo;
 	}
 }
---
+++ AngelMoreta_EvaluacionP3/ViewModels/AMDateViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngelMoreta_EvaluacionP3.ViewModels
+{
+    public class AMDateViewModel : INotifyPropertyChanged
+    {
+        DateTime _dateTime;
+        Timer _timer;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime DateTime
+        {
+            get => _dateTime;
+            set
+            {
+                if (_dateTime != value)
+                {
+                    _dateTime = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+        public AMDateViewModel()
+        {
+            this.DateTime = DateTime.Now;
+            _timer = new Timer(new TimerCallback((s) => this.DateTime = DateTime.Now), null, TimeSpan.Zero, TimeSpan.FromSeconds(1));
+        }
+
+        ~AMDateViewModel() => _timer.Dispose();
+        public void OnPropertyChanged([CallerMemberName] string name = "") => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+    }
+}
