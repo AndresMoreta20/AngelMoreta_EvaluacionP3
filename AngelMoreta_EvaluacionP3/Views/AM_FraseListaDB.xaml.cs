5db3f12a63ef71f723dc293b301c80a679040e98
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-02-01 09:09:18 -0500

    Se añade la pagina para ver la lista de frases en base de datos

--- AngelMoreta_EvaluacionP3/Views/AM_FraseListaDB.xaml.cs
+++ AngelMoreta_EvaluacionP3/Views/AM_FraseListaDB.xaml.cs
@@ -1,9 +1,42 @@
+using AngelMoreta_EvaluacionP3.Models;
+using AngelMoreta_EvaluacionP3.Views;
 namespace AngelMoreta_EvaluacionP3.Views;
 
 public partial class AM_FraseListaDB : ContentPage
 {
-	public AM_FraseListaDB()
-	{
-		InitializeComponent();
-	}
+    public AM_FraseListaDB()
+    {
+        InitializeComponent();
+        List<AMFrase> frase = App.AMFraseRepo.GetAllFrases();
+        AM_FraseList.ItemsSource = frase;
+        BindingContext = this;
+    }
+
+
+    public void OnItemAdded(object sender, EventArgs e)
+    {
+        // await Shell.Current.GoToAsync(nameof(BurgerItemPage));
+        Shell.Current.GoToAsync(nameof(AM_FraseItemPage), true, new Dictionary<string, object>
+        {
+            ["Item"] = new AMFrase()
+        });
+
+    }
+
+    public void OnItemSelected(object sender, SelectionChangedEventArgs e)
+    {
+        AMFrase selectedFrase = (AMFrase)AM_FraseList.SelectedItem;
+        if (selectedFrase != null)
+        {
+            Shell.Current.GoToAsync(nameof(AM_FraseItemPage), true, new Dictionary<string, object>
+            {
+                ["Item"] = selectedFrase
+            });
+
+        }
+        else
+        {
+            Console.WriteLine("error");
+        }
+    }
 }
