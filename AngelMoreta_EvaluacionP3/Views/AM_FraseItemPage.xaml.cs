6e86f9fd83e478e89607cc95c3925cb9928d4dd2
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-02-01 09:13:56 -0500

    se añade la pagina de cada item frase en la que se puede editar la frase, actualizar y borrar

--- AngelMoreta_EvaluacionP3/Views/AM_FraseItemPage.xaml.cs
+++ AngelMoreta_EvaluacionP3/Views/AM_FraseItemPage.xaml.cs
@@ -1,9 +1,81 @@
+using Newtonsoft.Json;
+using System.Net;
+using AngelMoreta_EvaluacionP3.Models;
+using AngelMoreta_EvaluacionP3.Views;
+using AngelMoreta_EvaluacionP3.ViewModels;
+
 namespace AngelMoreta_EvaluacionP3.Views;
 
+[QueryProperty("Item", "Item")]
 public partial class AM_FraseItemPage : ContentPage
 {
-	public AM_FraseItemPage()
-	{
-		InitializeComponent();
-	}
+
+    public AMFrase Item
+    {
+        get => BindingContext as AMFrase;
+        set => BindingContext = value;
+    }
+
+    bool _flag = false;
+    public AM_FraseItemPage()
+    {
+        InitializeComponent();
+        /* DateTime thisDay = DateTime.Today;
+         FechaActual.Text= "Fecha actual: "+ thisDay.ToString();*/
+        // string cadena = Buscador.Text;
+
+    }
+
+
+    private async void OnSaveClicked(object sender, EventArgs e)
+    {
+        Item.FechaActual = FechaActualV.Text;
+        App.AMFraseRepo.AddNewFrase(Item);
+        await Shell.Current.GoToAsync(nameof(AM_FraseListaDB));
+
+        /* Item.Name = nameB.Text;
+         Item.Description = descB.Text;
+         Item.WithExtraCheese = _flag;
+         App.BurgerRepo.AddNewBurger(Item);*/
+        //Shell.Current.GoToAsync("..");
+
+    }
+
+    private void OnCancelClicked(object sender, EventArgs e)
+    {
+        //Shell.Current.GoToAsync("..");
+        Shell.Current.GoToAsync(nameof(AM_FraseListaDB));
+    }
+
+    private void OnDeleteClicked(object sender, EventArgs e)
+    {
+        App.AMFraseRepo.DeleteFrase(Item);
+        Shell.Current.GoToAsync(nameof(AM_FraseListaDB));
+    }
+
+    public async void OnRandomClicked(object sender, EventArgs e)
+    {
+        var request = new HttpRequestMessage();
+        request.RequestUri = new Uri("https://api.quotable.io/random");
+        request.Method = HttpMethod.Get;
+        request.Headers.Add("Accept", "application/json"); var client = new HttpClient();
+        HttpResponseMessage response = await client.SendAsync(request);
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            String content = await response.Content.ReadAsStringAsync();
+            var resultado = JsonConvert.DeserializeObject<AMFrase>(content);
+            AM_Autor.Text = resultado.author;
+            AM_Contenido.Text = resultado.content;
+            Item._id = resultado._id;
+            Item.authorSlug = resultado.authorSlug;
+            Item.length = resultado.length;
+            Item.dateAdded = resultado.dateAdded;
+            Item.dateModified = resultado.dateModified;
+
+            // List<Frase> lista = new List<Frase>();
+            //  lista.Add(resultado);
+            // ListaDemo.ItemsSource = lista;
+        }
+    }
 }
