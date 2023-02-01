e11b07b05d3b69ff1b2eb02adc03070045a32c97
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-02-01 08:22:24 -0500

    Add project files.

---
+++ AngelMoreta_EvaluacionP3/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace AngelMoreta_EvaluacionP3;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AngelMoreta_EvaluacionP3/Platforms/Android/MainApplication.cs
@@ -0,0 +1,15 @@
+﻿using Android.App;
+using Android.Runtime;
+
+namespace AngelMoreta_EvaluacionP3;
+
+[Application]
+public class MainApplication : MauiApplication
+{
+	public MainApplication(IntPtr handle, JniHandleOwnership ownership)
+		: base(handle, ownership)
+	{
+	}
+
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
