19b94714986633129c55228bfbdfc6e9de5dba5e
AndresMoreta20 <andresmoreta2020@gmail.com>
2023-02-01 08:29:16 -0500

    Se añade la carpeta models con la clase Frase en la que se define al nombre y los campos de la tabla sqlite. Ademas se instalan los paquetes nuget.

---
+++ AngelMoreta_EvaluacionP3/Models/Frase.cs
@@ -0,0 +1,35 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AngelMoreta_EvaluacionP3.Models
+{
+    [Table("frase2")]
+    public class Frase
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string _id { get; set; }
+        public string author { get; set; }
+        public string content { get; set; }
+        // public List<string> tags { get; set; }
+        public string authorSlug { get; set; }
+        public int length { get; set; }
+        public string dateAdded { get; set; }
+        public string dateModified { get; set; }
+        public string FechaActual { get; set; }
+    }
+
+    public class Root
+    {
+        public int count { get; set; }
+        public int totalCount { get; set; }
+        public int page { get; set; }
+        public int totalPages { get; set; }
+        public int lastItemIndex { get; set; }
+        public List<Frase> results { get; set; }
+    }
+}
